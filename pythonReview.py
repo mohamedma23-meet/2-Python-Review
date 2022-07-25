492dc4357efcc18a5c200390499097af0f12b0b0
mohamedma23-meet <109899496+mohamedma23-meet@users.noreply.github.com>
2022-07-25 12:02:33 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,18 @@
+def creat_youtub_video(title, description):
+	youtub_video={"title":title,"description":description, "like":0 , "dislike":0, "comment"=comment{}}
+	return youtub_video
+
+def like(youtub_video):
+	if like in youtub_video:
+		like=like+1
+	return youtub_video
+
+def dislike(youtub_video):
+	if dislike in youtub_video:
+		dislike=dislike+1
+	return youtub_video
+
+def add_commoent (youtube_video, username, comment_text):
+	comment["username"]=username
+	comment["comment text"]=comment_text
+	return youtub_video
